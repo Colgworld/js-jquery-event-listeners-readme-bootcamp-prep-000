b4df37ad30834e3a122b69f9c631942391840acf
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-25 04:35:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,12 +12,9 @@ function frameIt() {
 }
 
 function submitIt() {
-$("form").on("submit", function() {
-  if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
-    return;
-  }
-    alert("you entered the wrong value");
-    return;
+    $("form").on("submit", function() {
+    $( "input:first" ).val() === "correct")
+
+return alert('your form is going to be submitted now');
   });
 }
